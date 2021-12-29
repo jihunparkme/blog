28ed9aa6a5a6338a6aec6ee6b8cd15a3f1fa69c8
Jihun Park <jihunpark.tech@gmail.com>
2021-12-29 11:44:39 +0900

    add krdict api test project

---
+++ projects/krdictApiTest/src/example/krdict/RequestEntity.java
@@ -0,0 +1,33 @@
+package example.krdict;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class RequestEntity {
+
+    private String key;
+    private String q;
+    private String translated;
+    private String transLang;
+    private String advanced;
+    private String method;
+
+    RequestEntity(String q) {
+        this.translated = "y"; //다국어 번역 여부
+        this.transLang = "1"; //영어
+        this.advanced = "y"; //자세히찾기 여부
+        this.method = "exact"; //일치 검색
+        this.q = q;
+    }
+
+    public String getParameter() {
+        return "?key=" + this.key +
+                "q=" + this.q +
+                "&advanced" + this.advanced +
+                "&method" + this.method +
+                "&translated" + this.translated +
+                "&trans_lang" + this.transLang;
+    }
+}
