aba3c8b7e58f6585ea12890c38ba7a5829edbf3d
Jihun Park <jihunpark.tech@gmail.com>
2025-04-03 19:44:02 +0900

    feat: init project

---
+++ projects/kafka-streams-example/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    kotlin("jvm") version "2.1.10"
+}
+
+group = "kafka-streams"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.apache.kafka:kafka-streams:4.0.0")
+    implementation("org.rocksdb:rocksdbjni:9.10.0")
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
---
+++ projects/kafka-streams-example/src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+package kafka
+
+-streams
+
+fun main() {
+    println("Hello World!")
+}
