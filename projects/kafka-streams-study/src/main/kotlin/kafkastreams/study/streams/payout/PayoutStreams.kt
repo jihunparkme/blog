17298139a8d968c05f5ecf30bc6e2878f49891c7
Jihun Park <jihunpark.tech@gmail.com>
2025-04-15 20:20:05 +0900

    test: commit test

--- projects/kafka-streams-study/src/main/kotlin/kafkastreams/study/streams/payout/PayoutStreams.kt
+++ projects/kafka-streams-study/src/main/kotlin/kafkastreams/study/streams/payout/PayoutStreams.kt
@@ -2,3 +2,14 @@ package kafkastreams.study.streams.payout
 
 class PayoutStreams {
 }
+
+/**
+ * - 소스 노드
+ * - 상태에서 룰 조회(ktable 활용 상태 관리)
+ * - 상태 테이블에 없을 경우 룰 조회
+ * - 업데이트 대상 필터링
+ * - 업데이트
+ * - 스트림 과정을 printed 로 확잉
+ * - 업데이트된 데이터를 누적
+ */
+
