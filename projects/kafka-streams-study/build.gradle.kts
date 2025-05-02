b9be2f850fe76134374934121f4d9b94b888c8ca
Jihun Park <jihunpark.tech@gmail.com>
2025-05-02 22:37:30 +0900

    [settlement kafka streams] feat: add spring-kafka dependency

--- projects/kafka-streams-study/build.gradle.kts
+++ projects/kafka-streams-study/build.gradle.kts
@@ -23,9 +23,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
 
+    implementation("org.springframework.kafka:spring-kafka")
     implementation("org.apache.kafka:kafka-streams:3.9.0")
     implementation("org.rocksdb:rocksdbjni:9.10.0")
-    implementation("org.slf4j:slf4j-simple:2.0.16")
     implementation("com.google.code.gson:gson:2.12.1")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
