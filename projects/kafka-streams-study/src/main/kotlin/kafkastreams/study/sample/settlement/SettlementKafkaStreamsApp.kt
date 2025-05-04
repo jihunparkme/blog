553024649faeb44b431f4634f7972b6a8e9c3212
Jihun Park <jihunpark.tech@gmail.com>
2025-05-04 22:21:28 +0900

    [settlement kafka streams] feat: edit comment

--- projects/kafka-streams-study/src/main/kotlin/kafkastreams/study/sample/settlement/SettlementKafkaStreamsApp.kt
+++ projects/kafka-streams-study/src/main/kotlin/kafkastreams/study/sample/settlement/SettlementKafkaStreamsApp.kt
@@ -4,6 +4,7 @@ import kafkastreams.study.sample.settlement.common.StreamMessage
 import kafkastreams.study.sample.settlement.config.KafkaProperties
 import kafkastreams.study.sample.settlement.config.KafkaStreamsConfig
 import kafkastreams.study.sample.settlement.domain.payment.Payment
+import kafkastreams.study.sample.settlement.service.SettlementService
 import org.apache.kafka.streams.KafkaStreams
 import org.apache.kafka.streams.StreamsBuilder
 import org.apache.kafka.streams.kstream.Printed
@@ -16,18 +17,18 @@ private const val APPLICATION_NAME = "settlement-streams"
 class SettlementKafkaStreamsApp(
     private val streamsConfig: KafkaStreamsConfig,
     private val kafkaProperties: KafkaProperties,
+    private val settlementService: SettlementService,
 ) {
     @Bean
     fun settlementStreams(): KafkaStreams {
         val builder = StreamsBuilder()
 
-        /**
-         * [소스 프로세서]
-         * - 결제 데이터 받기
-         * - 결제 로그 저장
-         */
+        // [소스 프로세서] 결제 토픽으로부터 결제 데이터 받기
         val paymentStream = builder.stream<String, StreamMessage<Payment>>(kafkaProperties.paymentTopic)
-        paymentStream.print(Printed.toSysOut<String, StreamMessage<Payment>>().withLabel("payment-stream"))
+        paymentStream
+            // [스트림 프로세서] 결제 메시지 로그 저장
+            .peek({ _, message -> settlementService.savePaymentMessageLog(message) })
+            .print(Printed.toSysOut<String, StreamMessage<Payment>>().withLabel("payment-stream"))
 
         /**
          * [스트림 프로세서]
---
+++ projects/kafka-streams-study/src/main/kotlin/kafkastreams/study/sample/settlement/service/SettlementService.kt
@@ -0,0 +1,18 @@
+package kafkastreams.study.sample.settlement.service
+
+import kafkastreams.study.common.logger
+import kafkastreams.study.sample.settlement.common.StreamMessage
+import kafkastreams.study.sample.settlement.domain.payment.Payment
+import org.springframework.stereotype.Service
+import kotlin.getValue
+
+@Service
+class SettlementService {
+    fun savePaymentMessageLog(data: StreamMessage<Payment>) {
+        log.info("📦Save payment message log to payment_log.. $data")
+    }
+
+    companion object {
+        private val log by logger()
+    }
+}
