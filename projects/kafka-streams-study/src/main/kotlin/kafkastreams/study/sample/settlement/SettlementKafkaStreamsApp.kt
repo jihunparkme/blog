d3228faa8e81b9d86ef1dae07bb82cae0292e3ef
Jihun Park <jihunpark.tech@gmail.com>
2025-05-07 23:00:22 +0900

    [settlement kafka streams] feat: add Consumed.with

--- projects/kafka-streams-study/src/main/kotlin/kafkastreams/study/sample/settlement/SettlementKafkaStreamsApp.kt
+++ projects/kafka-streams-study/src/main/kotlin/kafkastreams/study/sample/settlement/SettlementKafkaStreamsApp.kt
@@ -1,15 +1,20 @@
 package kafkastreams.study.sample.settlement
 
+import com.fasterxml.jackson.core.type.TypeReference
+import com.fasterxml.jackson.databind.ObjectMapper
 import kafkastreams.study.sample.settlement.common.StreamMessage
 import kafkastreams.study.sample.settlement.config.KafkaProperties
 import kafkastreams.study.sample.settlement.config.KafkaStreamsConfig
 import kafkastreams.study.sample.settlement.domain.payment.Payment
 import kafkastreams.study.sample.settlement.service.SettlementService
+import org.apache.kafka.common.serialization.Serdes
 import org.apache.kafka.streams.KafkaStreams
 import org.apache.kafka.streams.StreamsBuilder
+import org.apache.kafka.streams.kstream.Consumed
 import org.apache.kafka.streams.kstream.Printed
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.support.serializer.JsonSerde
 
 private const val APPLICATION_NAME = "settlement-streams"
 
@@ -18,13 +23,20 @@ class SettlementKafkaStreamsApp(
     private val streamsConfig: KafkaStreamsConfig,
     private val kafkaProperties: KafkaProperties,
     private val settlementService: SettlementService,
+    private val objectMapper: ObjectMapper,
 ) {
     @Bean
     fun settlementStreams(): KafkaStreams {
         val builder = StreamsBuilder()
 
         // [소스 프로세서] 결제 토픽으로부터 결제 데이터 받기
-        val paymentStream = builder.stream<String, StreamMessage<Payment>>(kafkaProperties.paymentTopic)
+        val paymentStream = builder.stream(
+            kafkaProperties.paymentTopic,
+            Consumed.with(
+                Serdes.String(),
+                JsonSerde(object : TypeReference<StreamMessage<Payment>>() {}, objectMapper)
+            )
+        )
         paymentStream
             // [스트림 프로세서] 결제 메시지 로그 저장
             .peek({ _, message -> settlementService.savePaymentMessageLog(message) })
